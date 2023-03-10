800592149b23c2c89dc1806ce77b06f402404f87
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-03-10 13:32:12 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import Profile from './profile/Profile';
 import user from '../data/user.json';
+import Statistics from './statistics/Statistics';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        flexDirection: 'column',
       }}
     >
       <Profile
@@ -19,6 +22,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       ></Profile>
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import css from './Statistics.module.css';
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li
+            style={{
+              backgroundColor: getRandomColor(),
+            }}
+            className={css.statItem}
+            key={stat.id}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Statistics;
